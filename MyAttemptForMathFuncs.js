efe5f096d5acfdce4c7e96be48622252348f0936
Prabesh Subedi <subediprabesh1@gmail.com>
2018-05-05 23:59:59 +0545

    Refactor Math Functions

--- MyAttemptForMathFuncs.js
+++ MyAttemptForMathFuncs.js
@@ -1,20 +1,12 @@
-function floor(num) {
-	const numStr = num.toString();
-	const nums = numStr.match(/\d+/g).map(Number);
-	return nums[0];
-}
-
-function ceil(num) {
-	const numStr = num.toString();
-	const nums = numStr.match(/\d+/g).map(Number);
-	return nums[1] ? ++nums[0] : nums[0];
-}
-
-function round(num) {
-	const numStr = num.toString();
-	const nums = numStr.match(/\d+/g).map(Number);
-	if (!nums[1]) nums[1] = 0;
-	const decimal = Number("." + nums[1].toString());
-	return decimal >= 0.5 ? ceil(num) : floor(num);
-}
-console.log(round(10));
+Math.floor = function(num) {
+	// removes all floating points;
+	return parseInt(num);
+};
+Math.ceil = function(num) {
+	// checks if floored num is equal to num and increments num accordingly
+	return parseInt(num) === num ? parseInt(num) : parseInt(num) + 1;
+};
+Math.round = function(num) {
+	// checks if the decimal place is greater than 0.5 and ceils or floors the num respectively
+	return num - parseInt(num) >= 0.5 ? parseInt(num) + 1 : parseInt(num);
+};
