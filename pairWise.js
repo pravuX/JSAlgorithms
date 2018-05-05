f906b8c9738cd0260900b15a410a7de95958e5e9
Prabesh Subedi <subediprabesh1@gmail.com>
2018-05-05 23:32:15 +0545

    Complete PairWise

--- pairWise.js
+++ pairWise.js
@@ -1,8 +1,29 @@
-//@ts-check
-function pairwise(arr, arg) {
-	const firstHalf = arr.slice(0, arr.length / 2);
-	const secondHalf = arr.slice(arr.length / 2);
-	console.log(firstHalf, secondHalf);
-	// My idea here is to check the firstHalf against the secondHalf and return those pairs that sum to arg into a neww array of objects(containing their indices);
-}
-pairwise([1, 4, 2, 3, 0, 5], 7);
+/*function pairwise(arr, arg) {
+	if (arr[0] == undefined) return 0;
+
+	const testArr = arr;
+	const indicesArr = [];
+	for (let i = 0; i < testArr.length; i++) {
+		for (let j = 0; j < testArr.length; j++) {
+			if (i !== j && testArr[i]+testArr[j] === arg) {
+				indicesArr.push(testArr.indexOf(testArr[i]));
+				delete testArr[i];
+				indicesArr.push(testArr.indexOf(testArr[j]));
+				delete testArr[j];	
+			}
+		}
+	}
+	return indicesArr.reduce((a, b) => a + b); // <== final solution
+}*/
+const pairwise = (arr, arg) => {
+	return arr.reduce((acc, cur, i, arr) => {
+		for (let x = i + 1; x < arr.length; x++) {
+			if (arr[i] + arr[x] === arg) {
+				acc += i + x;
+				arr[i] = arr[x] = NaN;
+			}
+		}
+		return acc;
+	}, 0);
+};
+console.log(pairwise([1, 1, 1], 2));
